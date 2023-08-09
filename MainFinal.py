7891e5e5bfff07d0ec9d65bca99e0452e83cebff
pgotthardtuno <141185069+pgotthardtuno@users.noreply.github.com>
2023-08-09 17:52:35 -0500

    Update MainFinal.py

--- MainFinal.py
+++ MainFinal.py
@@ -2,6 +2,10 @@ from logicFinal import *
 
 
 def main():
+    """
+    This function launches the main window powered by PyQt, the Logic class determines how the gui script is executed
+    :return:
+    """
     application = QApplication([])
     window = Logic()
     window.show()
